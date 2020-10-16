07131297a3d9fabd80cef618d5fbce8d09b9759c
Mackenzie Lim <macklim8@gmail.com>
2020-10-16 19:25:58 -0400

    Update schema-comments.sql

---
+++ app/data/schema-comments.sql
@@ -0,0 +1,16 @@
+Create Database msis;
+
+Use msis;
+
+DROP TABLE IF EXIST Comments;
+CREATE TABLE Comments (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  commentText TEXT NOT NULL
+);
+
+INSERT INTO Comments (id,commentText) VALUES
+("1", "This is the first comment"),
+("2", "Now this is the next comment yay"),
+("3", "Third times the charm!"),
+
+SELECT * FROM Comments
